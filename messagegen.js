826bd7f2d9eb35d83c29def02621e1bd729c6dc1
MemeLines <thebangoperator@proton.me>
2023-08-29 12:19:52 +0800

    first commit

---
+++ messagegen.js
@@ -0,0 +1,19 @@
+// Animal Joke Generator
+
+const animals = ["bear", "pig", "chimp", "dugong", "firefly", "buzzard"];
+const verbs = ['eating', 'approaching', 'licking', 'fighting', 'threatening'];
+const nouns = ["wife", "television", "grandpa", "children", "mouth"];
+
+const getRandomElement = (array) => {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+}
+const generateRandomSentence = () => {
+        const randomAnimal = getRandomElement(animals);
+        const randomVerb = getRandomElement(verbs);
+        const randomNoun = getRandomElement(nouns);
+        
+return `That damn ${randomAnimal} is ${randomVerb} my precious ${randomNoun}.`; 
+}
+console.log(generateRandomSentence())
+
